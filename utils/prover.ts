a16fa01b3c5b904e720079c313858ec8a7ad745d
Oliver Ryall <oliryall0404@gmail.com>
2023-04-03 09:26:55 +0100

    update: onmessage proof input

--- utils/prover.ts
+++ utils/prover.ts
@@ -9,9 +9,7 @@ onmessage = async event => {
     const { acir, input } = event.data;
 
     const [prover, verifier] = await setup_generic_prover_and_verifier(acir);
-    console.log(input);
-
-    const proof = await create_proof(prover, acir, input.values, input.query, input.signatureData);
+    const proof = await create_proof(prover, acir, input);
     postMessage(proof);
   } catch (er) {
     console.log(er);
